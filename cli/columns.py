f4c81cb400157fbfa9a593df292aaeeeb567b2ff
seanogary <seanogmoraczewski@gmail.com>
2026-02-13 11:00:52 -0500

    removed try, except in columns command as I do not need to explicitly propagate errors upward

--- cli/columns.py
+++ cli/columns.py
@@ -4,22 +4,16 @@ from .search_utils import match
 
 def columns_command(args):
 	max_width = 15
+	with load_data(args.file, args.delimiter) as reader:
+		first_row = next(reader)
+		col_counts = [0] * len(first_row)
+		for row in reader:
+			for index, entry in enumerate(row):
+				if entry:
+					col_counts[index]+=1
+		col_summary = [""] * len(col_counts)
+		for index, entry in enumerate(first_row):
+			entry += f" ({col_counts[index]})"
+			col_summary[index] = entry
 
-	try:
-		with load_data(args.file, args.delimiter) as reader:
-			first_row = next(reader)
-			col_counts = [0] * len(first_row)
-			for row in reader:
-				for index, entry in enumerate(row):
-					if entry:
-						col_counts[index]+=1
-			col_summary = [""] * len(col_counts)
-			for index, entry in enumerate(first_row):
-				entry += f" ({col_counts[index]})"
-				col_summary[index] = entry
-
-			print(format_row(col_summary, max_width, header=True))
-
-
-	except Exception:
-		raise
+		print(format_row(col_summary, max_width, header=True))
